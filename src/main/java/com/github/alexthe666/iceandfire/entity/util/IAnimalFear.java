55973761652b53616e3c20722a15260630db39da
afjl1919 <773499320@qq.com>
2026-07-15 11:17:47 +0800

    Delete src directory

--- src/main/java/com/github/alexthe666/iceandfire/entity/util/IAnimalFear.java
+++
@@ -1,7 +0,0 @@
-package com.github.alexthe666.iceandfire.entity.util;
-
-import net.minecraft.world.entity.Entity;
-
-public interface IAnimalFear {
-    boolean shouldAnimalsFear(Entity entity);
-}
